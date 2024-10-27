7215f5e505d2c96263f38aa0f170e49be189f269
qfbteam <durkmcteam@gmail.com>
2024-10-27 07:23:15 -0400

    Update Player.lua

--- hub2/Player.lua
+++ hub2/Player.lua
@@ -1,1 +1,10 @@
 local Library = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local Window = Fluent:CreateWindow({
+    Title = "Durk.lua"
+    SubTitle = "Local Player/by durk7896",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+})
