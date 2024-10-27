29c3d31d048267675dc98def3dcc40a03d216fa0
qfbteam <durkmcteam@gmail.com>
2024-10-27 07:46:37 -0400

    Update Player.lua

--- hub2/Player.lua
+++ hub2/Player.lua
@@ -3,8 +3,12 @@ local Window = Fluent:CreateWindow({
     Title = "Durk.lua"
     SubTitle = "Local Player/by durk7896",
     TabWidth = 160,
-    Size = UDim2.fromOffset(580, 460),
+    Size = UDim2.fromOffset(480, 360),
     Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
-    Theme = "Dark",
+    Theme = "Amethyst",
     MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
 })
+
+local Tabs = {
+    P = Window:AddTab({ Title = "Player", Icon = "code-xml" })
+}
